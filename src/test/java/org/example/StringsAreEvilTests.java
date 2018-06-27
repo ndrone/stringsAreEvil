1dbc0fd15d84f9a85aa6a5af8ae20be33c0a8c5b
Nicholas Drone <ndrone@gmail.com>
2018-06-26 21:18:36 -0400

    No List

--- src/test/java/org/example/StringsAreEvilTests.java
+++ src/test/java/org/example/StringsAreEvilTests.java
@@ -39,7 +39,8 @@ class StringsAreEvilTests {
         return Stream.of(Arguments.of(new BaselineParser(), "Baseline"),
                 Arguments.of(new EW1Parser(), "Easy Win 1"),
                 Arguments.of(new EW2Parser(), "Easy Win 2"),
-                Arguments.of(new NoSplitParser(), "No Line Split"));
+                Arguments.of(new NoSplitParser(), "No Line Split"),
+                Arguments.of(new NoListParser(), "No List"));
     }
 
     @ParameterizedTest
