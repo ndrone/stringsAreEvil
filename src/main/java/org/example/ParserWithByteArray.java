68c47c7727de2ddda496467b710fb61776bdba6d
Nicholas Drone <ndrone@gmail.com>
2018-06-26 21:28:42 -0400

    byte[]

---
+++ src/main/java/org/example/ParserWithByteArray.java
@@ -0,0 +1,51 @@
+package org.example;
+
+public class ParserWithByteArray implements LineParser {
+
+    @Override
+    public void parseLine(String line) {
+
+        if (line.startsWith("MNO")) {
+
+            byte[] commaIndexes = getCommaIndexes(line);
+            Integer elementId = parseSectionAsIntege(commaIndexes[0] + 1, commaIndexes[1], line);
+            Integer vehicleId = parseSectionAsIntege(commaIndexes[1] + 1, commaIndexes[2], line);
+            Integer term = parseSectionAsIntege(commaIndexes[2] + 1, commaIndexes[3], line);
+            Integer mileage = parseSectionAsIntege(commaIndexes[3] + 1, commaIndexes[4], line);
+            Double value = parseSectionAsDouble(commaIndexes[4] + 1, commaIndexes[5], line);
+            ValueHolder valueHolder = new ValueHolder(elementId, vehicleId, term, mileage, value);
+        }
+    }
+
+    private byte[] getCommaIndexes(String line) {
+        byte[] commaIndexes = new byte[7];
+        int counter = 0;
+        for (byte i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == ',') {
+                commaIndexes[counter++] = i;
+            }
+        }
+        return commaIndexes;
+    }
+
+
+    private Double parseSectionAsDouble(int start, int end, String line) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int index = start; index < end; index++) {
+            sb.append(line.charAt(index));
+        }
+
+        return Double.valueOf(sb.toString());
+    }
+
+    private Integer parseSectionAsIntege(int start, int end, String line) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int index = start; index < end; index++) {
+            sb.append(line.charAt(index));
+        }
+
+        return Integer.valueOf(sb.toString());
+    }
+}
--- src/test/java/org/example/StringsAreEvilTests.java
+++ src/test/java/org/example/StringsAreEvilTests.java
@@ -40,7 +40,8 @@ class StringsAreEvilTests {
                 Arguments.of(new EW1Parser(), "Easy Win 1"),
                 Arguments.of(new EW2Parser(), "Easy Win 2"),
                 Arguments.of(new NoSplitParser(), "No Line Split"),
-                Arguments.of(new NoListParser(), "No List"));
+                Arguments.of(new NoListParser(), "No List"),
+                Arguments.of(new ParserWithByteArray(), "byte[]"));
     }
 
     @ParameterizedTest
